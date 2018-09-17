a0336bc3974a40927fbf1a4341225afeaee412f2
Joao Sallaberry <joaovictorhs@gmail.com>
2018-09-16 22:48:18 -0300

    log requests

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true }))
 
+const fs = require('fs')
+const path = require('path')
+const morgan = require('morgan')
+
+// open mongodb connection
 const mongoose = require('mongoose')
 const config = require('./config/index')
 mongoose.set('useCreateIndex', true)
@@ -13,6 +18,11 @@ mongoose.connect(config.mongo.url, { useNewUrlParser: true })
 const requireDir = require('require-dir')
 const controllers = requireDir('./controllers')
 
+// log requests
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
+
+// define routes
 app.route('/rooms')
   .get(controllers.room.get)
   .post(controllers.room.create)
@@ -27,6 +37,7 @@ app.route('/bookings/:uid')
   .put(controllers.booking.update)
   .delete(controllers.booking.delete)
 
+// start server
 const port = process.env.PORT || 3000
 const server = app.listen(port, () =>
   console.log('Node server listening on port', server.address().port)
